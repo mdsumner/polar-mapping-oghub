d43f71c33177c0ca3fbdd9ec9b277af1f41812cd
Michael Sumner <mdsumner@gmail.com>
2019-08-14 03:39:11 +0000

    update data sets

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,4 +1,9 @@
 ## code to prepare `DATASET` dataset goes here
 aes_zone <- aceecostats::aes_zone
 aes_zone_ll <- aceecostats::aes_zone_ll
-save(aes_zone, aes_zone_ll, file = "data-raw/polar-mapping.Rdata", version = 2)
+sea_ice <- raadtools::readice("2019-08-06")[[1]]
+sea_temp <- raadtools::readsst(getZ(sea_ice), lon180 = FALSE)[[1]]
+
+save(aes_zone, aes_zone_ll,
+     sea_ice, sea_temp,
+     file = "data-raw/polar-mapping.Rdata", version = 2)
